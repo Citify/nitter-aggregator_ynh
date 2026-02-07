151c9dca54259a1e24396cf46818a457c04aa039
Citify <99703172+Citify@users.noreply.github.com>
2026-02-07 11:11:51 -0500

    Create _common.sh

---
+++ scripts/_common.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+phpversion=$(ynh_app_setting_get --app=$app --key=phpversion)
