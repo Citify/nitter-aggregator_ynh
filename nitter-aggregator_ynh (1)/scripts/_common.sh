ac00ddbd2897dd1a6a2ed02eb1bd48e3458c010d
Citify <99703172+Citify@users.noreply.github.com>
2026-02-07 11:40:33 -0500

    Delete nitter-aggregator_ynh (1) directory

--- nitter-aggregator_ynh (1)/scripts/_common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#=================================================
-# COMMON VARIABLES
-#=================================================
-
-phpversion=$(ynh_app_setting_get --app=$app --key=phpversion)
